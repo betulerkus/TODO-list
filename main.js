d57936ac8af704e383d1d6efc18a0075c19cbc4f
Betul Erkus <betulerkus@icloud.com>
2024-06-26 10:15:39 -0400

    edit

--- main.js
+++ main.js
@@ -1,263 +1,321 @@
 const newTodo = document.querySelector("#todoName");
-const addButton =document.querySelector("#todoAddButton");
-const searchTodo =document.querySelector("#todoSearch");
-const clearButton =document.querySelector("#todoClearButton");
-const addForm = (document.querySelectorAll(".card-body")[0]).children[0];
-const todoList = document.querySelectorAll(".card-body")[1].children[2];
-let todosInLocal =[];
-runEvents();
+const addButton = document.querySelector("#todoAddButton");
+const searchTodo = document.querySelector("#todoSearch");
+const clearButton = document.querySelector("#todoClearButton");
+const addForm = document.querySelector("#card-body1").children[0];
+const todoList = document.querySelector("#card-body2").children[1];
+let todosInLocal = [];
+runTodoListProject();
 
-function runEvents(){
-    
-    addForm.addEventListener("submit", addNewTodoFunc); 
-    searchTodo.addEventListener("keyup", searchTodoFunc);
-    todoList.addEventListener("click",iconsEvent);
-    clearButton.addEventListener("click", clearAllFunc);
-    document.addEventListener("DOMContentLoaded",getTodosFunc);
+
+function runTodoListProject() {
+
+  addForm.addEventListener("submit", addNewTodoFunc);
+  searchTodo.addEventListener("keyup", searchTodoFunc);
+  todoList.addEventListener("click", iconsEvent);
+  clearButton.addEventListener("click", clearAllFunc);
+  document.addEventListener("DOMContentLoaded", getTodosFunc);
 };
 
+
 function getTodosFunc() {
-    checkLocalStorage();
-    if (todosInLocal != "" && todosInLocal!=null){
-        setTodoList(todosInLocal);
-    }
+  checkLocalStorage();
+  if (todosInLocal != "" && todosInLocal != null) {
+    setTodoList(todosInLocal);
+  }
 }
 
-function setTodoList(todoArray){
-    todoArray.forEach(function(todo){
-        addTodoUI(todo);
-     })
+function setTodoList(todoArray) {
+  todoArray.forEach(function (todo) {
+    addTodoUI(todo);
+  })
 }
 function addNewTodoFunc(e) {
-    let todoValue= newTodo.value.trim();
-    if (todoValue == "" || todoValue==null){
-        alertPopUp("alert alert-warning", "Please write a new TO-DO");
-    }
-    else{
-        addTodoUI(todoValue);
-        addTodoLocalS(todoValue);
-        alertPopUp("alert alert-success", "Successfuly!!!");
-    }
-    newTodo.value = "";
-    e.preventDefault();
+  let todoValue = newTodo.value.trim();
+  if (todoValue == "" || todoValue == null) {
+    newTodo.value = "Please write a new TO-DO";
+    newTodo.style.color = "red";
+    newTodo.readOnly = "True";
+    setTimeout(function () {
+      newTodo.value = "";
+      newTodo.style.color = "";
+      newTodo.readOnly = "";
+    }, 1500);
+  }
+  else {
+    addTodoUI(todoValue);
+    addTodoLocalS(todoValue);
+
+    newTodo.value = "Successfuly!!!";
+    newTodo.style.color = "green";
+    newTodo.readOnly = "True";
+    newTodo.nextSibling.nextSibling.id = "wait"; //Add Botton Break!
+    setTimeout(function () {
+      newTodo.value = "";
+      newTodo.style.color = "";
+      newTodo.readOnly = "";
+    }, 1500);
+
+  }
+  e.preventDefault();
 }
 
-    // Add to UI
-    function addTodoUI(newValue){
-        //Create To-Do
-        const todoLi= document.createElement("li");
-        todoLi.className="list-group-item d-flex justify-content-between";
-        todoLi.style.border="0px";
-        const todoDiv1 = document.createElement("div");
-        todoDiv1.textContent=newValue.split(" ---",1);
+const alertDintFind = (alertText, parentElementAlert) => {
+  const alertLi = document.createElement("li")
+  alertLi.className = "list-group-item d-flex justify-content-between";
+  alertLi.style.flexDirection = "row";
+  alertLi.style.display = "flex";
+  alertLi.style.justifyContent = "space-between";
+  alertLi.style.alignItems = "center";
+  alertLi.style.paddingBottom = "1rem";
+  alertLi.style.paddingTop = "1rem";
+  alertLi.style.marginTop = "2rem";
 
-        const todoDiv2 = document.createElement("div");
+  const alertDiv = document.createElement("div");
+  alertDiv.style.flexDirection = "row";
+  alertDiv.style.display = "flex";
+  alertDiv.style.width = "100%";
+  const alertP = document.createElement("p");
+  alertP.textContent = alertText;
+  parentElementAlert.appendChild(alertLi).appendChild(alertDiv).appendChild(alertP);
+  // todoList.appendChild(alertLi).appendChild(alertDiv).appendChild(alertP);
+}
 
-        const todoCheck =document.createElement("input");
-        todoCheck.type="checkbox";
-        todoCheck.className="check"
-        
+// Add to UI
+function addTodoUI(newValue) {
+  //Create To-Do
 
-        const todoA2 =document.createElement("a");
-        todoA2.href="#";
-        todoA2.className="edit-item";
-        todoA2.style.marginLeft = "15px";
-        
-        const todoI2=document.createElement("i");
-        todoI2.className="fa fa-pencil";
 
-        const todoA3 =document.createElement("a");
-        todoA3.href="#";
-        todoA3.className="delete-item";
-        todoA3.style.marginLeft = "15px";
-        
-        const todoI3=document.createElement("i");
-        todoI3.className="fa fa-remove";
-            if (newValue.includes(" ---checked")) {
-            todoI2.style.color="#E8E8E8";
-            todoI3.style.color="#E8E8E8";
-            todoCheck.checked=true;
-            todoLi.style.textDecoration="line-through";
-            todoLi.style.color="#E8E8E8";
-            }
-        todoLi.appendChild(todoDiv1);
+  const todoLi = document.createElement("li");
+  todoLi.className = "list-group-item d-flex justify-content-between";
+  todoLi.style.flexDirection = "row";
+  todoLi.style.display = "flex";
+  todoLi.style.justifyContent = "space-between";
+  todoLi.style.alignItems = "center";
+  todoLi.style.paddingBottom = "0.5rem";
+  todoLi.style.paddingTop = "0.5rem";
+  todoLi.style.marginTop = "1rem";
+  todoLi.style.borderBottomWidth = "1px";
 
-        todoLi.appendChild(todoDiv2);
-        todoDiv2.appendChild(todoCheck);
-        todoDiv2.appendChild(todoA2).appendChild(todoI2);
-        todoDiv2.appendChild(todoA3).appendChild(todoI3);
-        todoList.appendChild(todoLi);
-    };
+  const todoDiv1 = document.createElement("div");
+  todoDiv1.style.flexDirection = "row";
+  todoDiv1.style.display = "flex";
+  todoDiv1.style.width = "90%";
+  const todoCheck = document.createElement("input");
+  todoCheck.type = "checkbox";
+  todoCheck.className = "check"
+  todoCheck.style.marginRight = "0.75rem";
+  const todoP1 = document.createElement("p");
+  todoP1.textContent = newValue.split(" ---", 1);
 
-    // Add to Local Storage
-    function addTodoLocalS(newValue) {
+  const todoDiv2 = document.createElement("div");
+  todoDiv2.style.width = "10%";
+  todoDiv2.style.flexDirection = "row";
+  todoDiv2.style.display = "flex";
+  todoDiv2.style.justifyContent = "flex-end";
+
+
+  const todoI2 = document.createElement("i");
+  todoI2.className = "bx bx-pencil";
+
+  const todoI3 = document.createElement("i");
+  todoI3.className = "bx bx-trash";
+  todoI3.style.marginLeft = "0.75rem";
+
+  if (newValue.includes(" ---checked")) {
+    todoCheck.checked = true;
+    todoLi.style.textDecoration = "line-through";
+    todoLi.style.color = "#E8E8E8";
+  }
+  todoLi.appendChild(todoDiv1);
+  todoLi.appendChild(todoDiv2);
+  todoDiv1.appendChild(todoCheck);
+  todoDiv1.appendChild(todoP1);
+  todoDiv2.appendChild(todoI2);
+  todoDiv2.appendChild(todoI3);
+  todoList.appendChild(todoLi);
+};
+
+// Add to Local Storage
+function addTodoLocalS(newValue) {
+  checkLocalStorage();
+  // todosInLocal.push("{todo: \""+newValue+"\", durum: \" unchecked\"}");
+  todosInLocal.push(newValue + " ---unchecked");
+
+  localStorage.setItem("todosInLocal", JSON.stringify(todosInLocal));
+}
+
+//Check the LocalStorage
+function checkLocalStorage() {
+  if (localStorage.getItem("todosInLocal") === null) {
+    todosInLocal = [];
+  }
+  else {
+    todosInLocal = JSON.parse(localStorage.getItem("todosInLocal"));
+  }
+}
+
+function searchTodoFunc() {
+  let keyWord = searchTodo.value;
+  checkLocalStorage();
+  if (todosInLocal != "" && todosInLocal != null) {
+    let findTodoList = [];
+    todosInLocal.forEach(function (todo) {
+      if (todo.toUpperCase().includes(keyWord.toUpperCase())) {
+        findTodoList.push(todo);
+      }
+    })
+    while (todoList.hasChildNodes()) {
+      todoList.removeChild(todoList.firstChild)
+    }
+
+    if (findTodoList == "" || findTodoList == null) {
+      let parentElement = todoList;
+      alertDintFind("There aren't  TO DO includes \"" + keyWord + "\"", parentElement);
+    }
+    else {
+      setTodoList(findTodoList);
+    }
+  }
+}
+
+function iconsEvent(e) {
+  switch (e.target.className) {
+    case "check":
+      //  When Checked - Mark as TODO
+      // In UI
+      if (e.target.checked == false) {
+
+        unCheckedTodoUI(e.target);
+        //IN Local Storage
+        const todoText = e.target.nextSibling.textContent;
+        checkLocalStorage()
+        for (let i = 0; i < todosInLocal.length; i++) {
+          if (todosInLocal[i].split(" ---", 1) == todoText) {
+            todosInLocal[i] = todosInLocal[i].split(" ---", 1) + " ---unChecked";
+            localStorage.setItem("todosInLocal", JSON.stringify(todosInLocal));
+            break;
+          }
+
+        }
+      }
+      // When Unchecked - Mark as done
+      else {
+        // In UI
+        checkedTodoUI(e.target);
+        //IN Local Storage
+        const todoText = e.target.nextSibling.textContent;
+        checkLocalStorage()
+        for (let i = 0; i < todosInLocal.length; i++) {
+          if (todosInLocal[i].split(" ---", 1) == todoText) {
+            todosInLocal[i] = todosInLocal[i].split(" ---", 1) + " ---checked";
+            localStorage.setItem("todosInLocal", JSON.stringify(todosInLocal))
+            break;
+          }
+
+        }
+      }
+
+
+      break;
+    case "bx bx-pencil":
+      let editIcon = e.target;
+      let target = e.target.parentElement.previousSibling;
+      let oldText = e.target.parentElement.previousSibling.children[1].textContent;
+      target.children[1].style.display = "none"; //oldText display none
+      editIcon.style.display = "none"; //edit icon display none
+
+      // Set edit form
+      const form = document.createElement("form")
+      form.className = "editTodoForm";
+      form.style.width = "100%";
+      form.style.borderRadius = "0.25rem";
+      form.style.justifyContent = "space-between";
+      form.style.boxShadow = "1px 0.5px 20px 0.1px #e879f9";
+      const editInput = document.createElement("input");
+      editInput.type = "text";
+      editInput.value = oldText;
+      editInput.style.padding = "2px";
+      editInput.style.width = "85%";
+      editInput.style.borderTopLeftRadius = "0.25rem";
+      editInput.style.borderBottomLeftRadius = "0.25rem";
+      editInput.className = "edit-input";
+      const editTodoButton = document.createElement("input");
+      editTodoButton.type = "submit";
+      editTodoButton.style.padding = "2px";
+      editTodoButton.style.backgroundColor = "#e879f9";
+      editTodoButton.style.justifyContent = "flex-end";
+      editTodoButton.style.width = "15%";
+      editTodoButton.style.color = "white";
+      editTodoButton.style.borderTopRightRadius = "0.25rem";
+      editTodoButton.style.borderBottomRightRadius = "0.25rem";
+      editTodoButton.value = "Save";
+      target.appendChild(form);
+      form.appendChild(editInput);
+      form.appendChild(editTodoButton);
+      editInput.focus();
+
+      // edit function
+      form.addEventListener("submit", editTodoFunc);
+      function editTodoFunc(e) {
         checkLocalStorage();
-        // todosInLocal.push("{todo: \""+newValue+"\", durum: \" unchecked\"}");
-        todosInLocal.push(newValue+" ---unchecked");
-
+        let newText = editInput.value;
+        for (let i = 0; i < todosInLocal.length; i++) {
+          if (todosInLocal[i].split(" ---", 1) == oldText) {
+            todosInLocal[i] = todosInLocal[i].replace(oldText, newText);
+          }
+        }
+        target.children[1].style.display = "block";
+        target.children[1].innerHTML = newText;
+        form.style.display = "none";
+        editIcon.style.display = "block";
         localStorage.setItem("todosInLocal", JSON.stringify(todosInLocal));
-    }
+        e.preventDefault();
+      }
 
-    //Check the LocalStorage
-    function checkLocalStorage(){
-        if (localStorage.getItem("todosInLocal")===null) {
-         todosInLocal=[];
+      break;
+    case "bx bx-trash":
+      //Delete from UI
+      //SUCCESSFUL message
+      e.target.parentElement.previousSibling.children[1].textContent = "To Do deleted";
+      e.target.parentElement.parentElement.style.backgroundColor = "rgb(134 239 172)";
+
+      setTimeout(() => {
+        e.target.parentElement.parentElement.remove();
+      }, 1000);
+
+
+      //Delete from LocalStorage
+      const removeText = e.target.parentElement.previousSibling.children[1].textContent;
+      const todosLocal = JSON.parse(localStorage.getItem("todosInLocal"));
+      todosLocal.forEach(function (item, index, object) {
+        if (item.includes(removeText)) {
+          object.splice(index, 1);
         }
-        else{
-        todosInLocal = JSON.parse(localStorage.getItem("todosInLocal"));
-        }
-    }
+      })
+      localStorage.setItem("todosInLocal", JSON.stringify(todosLocal));
 
-    //Show Alert Pop-Up
-    function alertPopUp(classAlert, textAlert){
-        const popupDiv = document.createElement("div");
-        popupDiv.className=classAlert;
-        popupDiv.role="alert";
-        popupDiv.textContent=textAlert;
-        popupDiv.style.marginTop="20px";
-
-        addForm.appendChild(popupDiv);
-        // document.querySelector("#hr").after(popupDiv);
-
-        setTimeout(function() {
-            popupDiv.remove();
-        }, 1500);
-    }
-    
-
-function searchTodoFunc(){
-    const keyWord = searchTodo.value;
-    checkLocalStorage();
-    if (todosInLocal !="" && todosInLocal!= null){
-        let findTodoList=[];
-        todosInLocal.forEach(function (todo) {
-            if (todo.toUpperCase().includes(keyWord.toUpperCase())) {
-                findTodoList.push(todo);
-            }
-        })
-        while (todoList.hasChildNodes()) {
-            todoList.removeChild(todoList.firstChild)
-        }
-        setTodoList(findTodoList);
-        
-    }
+  }
 }
 
-function iconsEvent(e){
-    switch (e.target.className) {
-        case "check":
-        //  When Checked - Mark as TODO
-            // In UI
-            if (e.target.checked==false) {
+function clearAllFunc() {
+  let sure = confirm("Are you sure clear all?");
+  if (sure) {
+    // Clear from LocalStorage 
+    localStorage.clear();
 
-                unCheckedTodoUI(e.target);
-            //IN Local Storage
-            const todoText = e.target.parentElement.previousSibling.textContent;
-            checkLocalStorage()
-            for (let i = 0; i < todosInLocal.length; i++) {
-                if (todosInLocal[i].split(" ---",1)==todoText) {
-                    todosInLocal[i]= todosInLocal[i].split(" ---",1)+" ---unChecked";
-                    localStorage.setItem("todosInLocal",JSON.stringify(todosInLocal));
-                    break;
-                }
-                
-            }
-        }
-        // When Unchecked - Mark as done
-        else{
-            // In UI
-            checkedTodoUI (e.target);
-            //IN Local Storage
-            const todoText = e.target.parentElement.previousSibling.textContent;
-            checkLocalStorage()
-            for (let i = 0; i < todosInLocal.length; i++) {
-                if (todosInLocal[i].split(" ---",1)==todoText) {
-                    todosInLocal[i]= todosInLocal[i].split(" ---",1)+" ---checked";
-                    localStorage.setItem("todosInLocal",JSON.stringify(todosInLocal))
-                    break;
-                }
-                
-            }
-        }
-
-
-          break;
-        case "fa fa-pencil":
-            let target =e.target.parentElement.parentElement.previousSibling;
-            let oldText =e.target.parentElement.parentElement.previousSibling.textContent;
-            e.target.parentElement.parentElement.previousSibling.textContent = "";
-
-            // Set edit form
-            const form = document.createElement("form")
-            form.className="editTodoForm";
-            const editInput = document.createElement("input");
-            editInput.type="text";
-            editInput.value = oldText;
-            editInput.className = "edit-input";
-            const editTodoButton = document.createElement("input");
-            editTodoButton.type="submit";
-            editTodoButton.value="Save";
-            target.appendChild(form);
-            form.appendChild(editInput);
-            form.appendChild(editTodoButton);
-
-            // edit function
-            form.addEventListener("submit", editTodoFunc);
-            function editTodoFunc(){
-                let newText = editInput.value;
-                for (let i = 0; i < todosInLocal.length; i++) {
-                    if (todosInLocal[i]==oldText)
-                        {todosInLocal[i]=newText;
-                        }
-                }
-                target.textContent=newText;
-                localStorage.setItem("todosInLocal", JSON.stringify(todosInLocal));
-            }
-            
-          break;
-        case "fa fa-remove":
-            //Delete from UI
-            e.target.parentElement.parentElement.parentElement.remove();
-
-            //Delete from LocalStorage
-            const removeText = e.target.parentElement.parentElement.previousSibling.textContent;
-            const todosLocal = JSON.parse(localStorage.getItem("todosInLocal"));
-            todosLocal.forEach(function(item, index, object){
-                if (item===removeText) {
-                    object.splice(index, 1);
-                }
-            })
-            localStorage.setItem("todosInLocal", JSON.stringify(todosLocal));
-
-    }
+    //Clear from UI
+    location.reload();
+  }
 }
 
-function clearAllFunc(){
-    let sure = confirm("Are you sure clear all?");
-    if (sure) {
-        // Clear from LocalStorage 
-        localStorage.clear();
-        
-        //Clear from UI
-        while (todoList.childNodes.length>1) {
-            todoList.firstElementChild.remove();    
-        }
-    }
-    
+function checkedTodoUI(e) {
+  (e.parentElement.parentElement).style.textDecoration = "line-through";
+  (e.parentElement.parentElement).style.color = "#E8E8E8";
 }
 
-function checkedTodoUI (e){
-    (e.parentElement.parentElement).style.textDecoration = "line-through";
-    (e.parentElement.parentElement).style.color = "#E8E8E8";
-    (e).style.color="#E8E8E8";
-    e.nextSibling.firstChild.style.color="#E8E8E8";
-    e.nextSibling.nextSibling.firstChild.style.color="#E8E8E8";
+function unCheckedTodoUI(e) {
+  (e.parentElement.parentElement).style.textDecoration = "";
+  (e.parentElement.parentElement).style.color = "";
 }
 
-function unCheckedTodoUI(e){
-    (e.parentElement.parentElement).style.textDecoration="";
-    (e.parentElement.parentElement).style.color = "";
-    e.style.color="";
-    e.nextSibling.firstChild.style.color="";
-    e.nextSibling.nextSibling.firstChild.style.color="";
-}
